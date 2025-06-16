ab3672c7f1499e1ca406d82a0cfffc304417fda6
kggrowthcenter <data@growthcenter.id>
2025-06-16 15:42:38 +0700

    Create query_lestari.sql

---
+++ query_lestari.sql
@@ -0,0 +1,14 @@
+select 
+u.email as email, 
+u.full_name as name, 
+MAX(cu.created_at) as enroll_date, 
+MAX(cu.updated_at) as last_update, 
+c.title as title, 
+ROUND(SUM(cup.progress_duration),0) as duration, 
+AVG(cup.progress_percentage) as progress
+from course_users cu
+left join courses c on cu.course_serial = c.serial 
+left join users u on cu.user_serial = u.serial 
+left join partner_groups pg on cu.partner_group_serial = pg.serial
+left join course_user_progress cup on cup.course_serial = c.serial and cup.user_serial = u.serial
+group by u.email, u.full_name, c.title 
