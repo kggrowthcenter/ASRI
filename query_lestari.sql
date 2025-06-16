410b3ebf10ed22560163bd759b6483be63d5d8d4
kggrowthcenter <data@growthcenter.id>
2025-06-17 00:37:05 +0700

    Update query_lestari.sql

--- query_lestari.sql
+++ query_lestari.sql
@@ -4,6 +4,7 @@ u.full_name as name,
 MAX(cu.created_at) as enroll_date, 
 MAX(cu.updated_at) as last_update, 
 c.title as title, 
+c2.name as category,
 ROUND(SUM(cup.progress_duration),0) as duration, 
 AVG(cup.progress_percentage) as progress
 from course_users cu
@@ -11,4 +12,5 @@ left join courses c on cu.course_serial = c.serial
 left join users u on cu.user_serial = u.serial 
 left join partner_groups pg on cu.partner_group_serial = pg.serial
 left join course_user_progress cup on cup.course_serial = c.serial and cup.user_serial = u.serial
-group by u.email, u.full_name, c.title 
+left join categories c2 on c2.serial  = c.category_serial 
+group by u.email, u.full_name, c.title, c2.name 
