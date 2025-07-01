e2ef9413c8ed8b35dab200dcef20116b3161505b
kggrowthcenter <data@growthcenter.id>
2025-07-01 14:18:01 +0700

    Update query_asri.sql

--- query_asri.sql
+++ query_asri.sql
@@ -1,5 +1,6 @@
 select 
 cp.created_at as tanggal_daftar,
+cp.updated_at as last_update,
 cp.serial as serial_cp,
 cp.first_name as nama_pendaftar, 
 cp.email as email,
@@ -12,9 +13,9 @@ cp.school_district,
 cp.school_subdistrict, 
 cp.school_province,
 cpm.serial as serial_cpm,
-cpm.`role` as role_terdaftar, 
-cpm.grade, 
-cpm.full_name as nama_terdaftar,
+cpm.`role` as role_peserta, 
+cpm.grade as grade, 
+cpm.full_name as peserta,
 cpm.phone_number as no_tlp_student
 from competition_participants cp 
 left join competition_participant_members cpm on cpm.competition_participant_serial = cp.serial
