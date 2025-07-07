978cfc13be760f2f34780473e4ebd24c94741658
kggrowthcenter <data@growthcenter.id>
2025-07-07 21:44:20 +0700

    Update data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -3,19 +3,19 @@ import pandas as pd
 import numpy as np
 from fetch_data import fetch_data_asri, fetch_data_lestari, fetch_creds
 
-@st.cache_data(ttl=86400)
+@st.cache_data(ttl=1800)
 def finalize_data_asri():
     df_asri = fetch_data_asri()
     df_asri['tanggal_daftar'] = pd.to_datetime(df_asri['tanggal_daftar'], format="%Y-%m-%d").dt.date
     return df_asri
 
-@st.cache_data(ttl=86400)
+@st.cache_data(ttl=1800)
 def finalize_data_lestari():
     df_lestari = fetch_data_lestari()
     df_lestari['last_update'] = pd.to_datetime(df_lestari['last_update'], format="%Y-%m-%d").dt.date
     return df_lestari
 
-@st.cache_data(ttl=86400)
+@st.cache_data(ttl=1800)
 def finalize_data():
     df_asri = finalize_data_asri()
     df_lestari = finalize_data_lestari()
