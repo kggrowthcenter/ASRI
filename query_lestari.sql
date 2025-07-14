ea8b5f091ad50e87d24f3f5810ea3ba94b44a31f
kggrowthcenter <data@growthcenter.id>
2025-07-14 14:05:20 +0700

    Update query_lestari.sql

--- query_lestari.sql
+++ query_lestari.sql
@@ -1,16 +1,20 @@
-select 
-u.email as email, 
-u.full_name as name, 
-MAX(cu.created_at) as enroll_date, 
-MAX(cu.updated_at) as last_update, 
-c.title as title, 
-c2.name as category,
-ROUND(SUM(cup.progress_duration),0) as duration, 
-AVG(cup.progress_percentage) as progress
-from course_users cu
-left join courses c on cu.course_serial = c.serial 
-left join users u on cu.user_serial = u.serial 
-left join partner_groups pg on cu.partner_group_serial = pg.serial
-left join course_user_progress cup on cup.course_serial = c.serial and cup.user_serial = u.serial
-left join categories c2 on c2.serial  = c.category_serial 
-group by u.email, u.full_name, c.title, c2.name 
+SELECT 
+  u.serial as serial,
+  u.email AS email, 
+  u.full_name AS name, 
+  u.created_at AS regis_date,
+  MAX(cu.created_at) AS enroll_date,
+  MAX(cu.updated_at) AS last_update,
+  c.title AS title, 
+  c2.name AS category,
+  ROUND(SUM(cup.progress_duration), 0) AS duration, 
+  AVG(cup.progress_percentage) AS progress
+FROM users u
+LEFT JOIN course_users cu ON cu.user_serial = u.serial
+LEFT JOIN courses c ON cu.course_serial = c.serial 
+LEFT JOIN partner_groups pg ON cu.partner_group_serial = pg.serial
+LEFT JOIN course_user_progress cup 
+  ON cup.course_serial = cu.course_serial AND cup.user_serial = u.serial
+LEFT JOIN categories c2 ON c2.serial = c.category_serial 
+GROUP BY u.email, u.full_name, u.created_at, c.title, c2.name
+ORDER BY regis_date DESC;
