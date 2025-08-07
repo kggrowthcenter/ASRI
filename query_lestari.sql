dee5b12a2984ae2abb9dd5595ed0663556b61265
kggrowthcenter <data@growthcenter.id>
2025-08-07 13:43:12 +0700

    Update query_lestari.sql

--- query_lestari.sql
+++ query_lestari.sql
@@ -1,18 +1,19 @@
 SELECT 
-  u.serial as serial,
-  u.email AS email, 
-  u.full_name AS name, 
-  u.created_at AS regis_date,
-  u.last_login AS last_login ,
-  MAX(cu.created_at) AS enroll_date,
-  MAX(cu.updated_at) AS last_update,
-  c.title AS title, 
-  c2.name AS category,
-  ROUND(SUM(cup.progress_duration), 0) AS duration, 
-	CASE
-		WHEN cu.accomplished_at IS NULL THEN 'In Progress'
-		WHEN cu.accomplished_at IS NOT NULL THEN 'Finished'
-	END AS 'course_status'
+u.serial as serial,
+u.email AS email, 
+u.full_name AS name, 
+u.created_at AS regis_date,
+u.last_login AS last_login ,
+MAX(cu.created_at) AS enroll_date,
+MAX(cu.updated_at) AS last_update,
+c.title AS title, 
+c2.name AS category,
+ROUND(SUM(cup.progress_duration), 0) AS duration,
+ROUND(AVG(cup.progress),0) as progress,
+CASE
+	WHEN cu.accomplished_at IS NULL THEN 'In Progress'
+	WHEN cu.accomplished_at IS NOT NULL THEN 'Finished'
+END AS 'course_status'
 FROM users u
 LEFT JOIN course_users cu ON cu.user_serial = u.serial
 LEFT JOIN courses c ON cu.course_serial = c.serial 
