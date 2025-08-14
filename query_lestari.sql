902ebcfa3450e1d4ee875f94ff6df0426cd3ac4d
kggrowthcenter <data@growthcenter.id>
2025-08-14 18:30:28 +0700

    Update query_lestari.sql

--- query_lestari.sql
+++ query_lestari.sql
@@ -1,16 +1,16 @@
-SELECT
-  u.serial AS serial,
-  u.email AS email,
-  u.full_name AS name,
+SELECT 
+  u.serial as serial,
+  u.email AS email, 
+  u.full_name AS name, 
   u.created_at AS regis_date,
   u.last_login AS last_login,
   MAX(cu.created_at) AS enroll_date,
   MAX(cu.updated_at) AS last_update,
-  c.title AS title,
+  c.title AS title, 
   c2.name AS category,
-  ROUND(SUM(cup.progress_duration), 0) AS duration,
+  ROUND(SUM(cup.progress_duration), 0) AS duration, 
   AVG(cup.progress_percentage) AS progress,
-  CASE
+    CASE
     WHEN cu.accomplished_at IS NULL THEN 'In Progress'
     ELSE 'Finished'
   END AS course_status,
@@ -31,15 +31,16 @@ SELECT
   END AS total_duration,
   pgm.status AS playlist_status
 FROM users u
-LEFT JOIN course_user_progress cup ON u.serial = cup.user_serial
-LEFT JOIN courses c ON cup.course_serial = c.serial
+LEFT JOIN course_users cu ON cu.user_serial = u.serial
+LEFT JOIN courses c ON cu.course_serial = c.serial 
+LEFT JOIN partner_groups pg ON cu.partner_group_serial = pg.serial
+LEFT JOIN course_user_progress cup ON cup.course_serial = cu.course_serial AND cup.user_serial = u.serial
 LEFT JOIN course_sections cs ON cup.course_section_serial = cs.serial
 LEFT JOIN course_contents cc ON cup.course_content_serial = cc.serial
 LEFT JOIN course_user_quiz_answers cuqa ON cuqa.course_content_serial = cup.course_content_serial AND cuqa.user_serial = cup.user_serial
-LEFT JOIN course_users cu ON cu.course_serial = cup.course_serial AND cu.user_serial = cup.user_serial
+LEFT JOIN categories c2 ON c2.serial = c.category_serial 
 LEFT JOIN partner_playlists pp ON cup.partner_playlist_serial = pp.serial
 LEFT JOIN partner_group_members pgm ON cup.user_serial = pgm.user_serial AND cup.partner_group_serial = pgm.partner_group_serial
-LEFT JOIN categories c2 ON c2.serial = c.category_serial
 GROUP BY
   u.serial,
   u.email,
@@ -51,4 +52,4 @@ GROUP BY
   cu.accomplished_at,
   c.serial,
   pgm.status
-ORDER BY regis_date DESC;
+ORDER BY name DESC;
